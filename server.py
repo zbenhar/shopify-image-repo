d41fa361130e5ffeb705395c0e0960178a6b67cd
Zakaria Ben-Harchache <zakbenhar@Zaks-MacBook-Pro.local>
2021-09-20 22:36:09 -0700

    image repo code

---
+++ server.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+def get_cursor():
+    conn = sql.connect("database.db")
+    cur = conn.cursor()
+    return (cur, conn)
+
+"""Initialize the sqlite database and fill up the `products` table with sample data."""
+def initialize_db():
+    (cur, conn) = get_cursor()
+
+    # Create products table with sample data
+    cur.execute("DROP TABLE IF EXISTS products")
+    cur.execute("CREATE TABLE products (name TEXT, imgpath TEXT, price INTEGER, stock INTEGER)")
+    cur.execute("""INSERT INTO products (name, imgpath, price, stock) VALUES \
+        ('Nike shoes', 'images/nike-shoes.png', 20000, 8), \
+        ('iPhone', 'images/iphone.png', 100000, 2), \
+        ('Succulent', 'images/succulent.png', 3500, 34), \
+        ('Office chair', 'images/chair.png', 32500, 0), \
+        ('Smart watch', 'images/watch.png', 57900, 29)
+    """)
+
+    # Create empty transactions table
+    cur.execute("DROP TABLE IF EXISTS transactions")
+    cur.execute("CREATE TABLE transactions (timestamp TEXT, productid INTEGER, value INTEGER)")
+    
+    # Commit the db changes
+    conn.commit()
+    print("Initialized database")
+
+@app.route("/")
+def home_page():
+    (cur, _) = get_cursor()
+    cur.execute("SELECT rowid, * FROM products")
+    
+    rows = cur.fetchall()
+    print("Retrieved %d database entries" % len(rows))
+    
+    # Pre-process product info for HTML templates
+    products = []
+    for row in rows:
+        products.append({
+            "id":    row[0],
+            "name":  row[1],
+            "src":   "/static/%s" % (row[2]),
+            "price": "$%.2f" % (row[3]/100.0),
+            "stock": "%d left" % (row[4]),
+        })
+    
+    # Display total sales so far
+    cur.execute("SELECT SUM(value) FROM transactions")
+    result = cur.fetchone()[0]
+    earnings = result/100.0 if result else 0.00
+
+    return render_template("index.html", products=products, earnings=earnings)
+
+@app.route("/buy/<product_id>")
+def buy(product_id):
+    if not product_id:
+        return render_template("message.html", message="Invalid product ID!")
+
+    (cur, conn) = get_cursor()
+
+    cur.execute("SELECT rowid, price, stock FROM products WHERE rowid = ?", (product_id,))
+    result = cur.fetchone()
+
+    if not result:
+        return render_template("message.html", message="Invalid product ID!")
+    (rowid, price, stock) = result
+
+    if stock <= 0:
+        return render_template("message.html", message="Insufficient stock!")
+
+    print("Processed transaction of value $%.2f" % (price/100.0))
+    cur.execute("INSERT INTO transactions (timestamp, productid, value) VALUES " + \
+        "(datetime(), ?, ?)", (rowid, price))
+
+    cur.execute("UPDATE products SET stock = stock - 1 WHERE rowid = ?", (product_id,))
+    conn.commit()
+    return render_template("message.html", message="Purchase successful!")
+    
+
+@app.route("/reset")
+def reset():
+    initialize_db()
+    return render_template("message.html", message="Database reset.")
+
+if __name__ == '__main__':
+    initialize_db()
+    app.run(debug = True)
